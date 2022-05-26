b19a57ddb567e6a2dc89f780721c888fe8e723ce
Fernando Marins <f.augustomarins@gmail.com>
2022-05-26 11:17:57 -0300

    first commit

---
+++ wordle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wordle
+//
+//  Created by Fernando Marins on 26/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
