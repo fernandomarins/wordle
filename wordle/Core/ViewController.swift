6c116270b9627178b4612895360673d8d371e25b
Fernando Marins <f.augustomarins@gmail.com>
2022-05-26 12:38:11 -0300

    added the child view controllers

--- wordle/Core/ViewController.swift
+++ wordle/Core/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let keyboardViewController = KeyboardViewController()
+    let boardViewController = BoardViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemGray6
+        addChildren()
     }
 
-
+    private func addChildren() {
+        addChild(keyboardViewController)
+        keyboardViewController.didMove(toParent: self)
+        keyboardViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(keyboardViewController.view)
+        
+        addChild(boardViewController)
+        boardViewController.didMove(toParent: self)
+        boardViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(boardViewController.view)
+        
+        addConstraints()
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            boardViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            boardViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            boardViewController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            boardViewController.view.bottomAnchor.constraint(equalTo: keyboardViewController.view.topAnchor),
+            boardViewController.view.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.6),
+            
+            keyboardViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            keyboardViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            keyboardViewController.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 }
 
