e5151a4272a2c2390d86fbb8d6853793af90cc94
Fernando Marins <f.augustomarins@gmail.com>
2022-05-27 08:52:10 -0300

    displaying the board

--- wordle/Core/BoardViewController.swift
+++ wordle/Core/BoardViewController.swift
@@ -7,12 +7,73 @@
 
 import UIKit
 
-class BoardViewController: UIViewController {
+class BoardViewController:  UIViewController {
+    
+    var guesses: [[Character]] = Array(repeating: Array(repeating: "B", count: 5), count: 6)
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 4
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .clear
+        collectionView.register(KeyCell.self, forCellWithReuseIdentifier: KeyCell.identifier)
+        
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
-    }
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 35),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -35),
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 30),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
 
+    }
+}
+
+extension BoardViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: KeyCell.identifier,
+                                                            for: indexPath) as? KeyCell else { fatalError() }
+
+        cell.backgroundColor = nil
+        cell.layer.borderWidth = 1
+        cell.layer.borderColor = UIColor.systemGray3.cgColor
+        return cell
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return guesses.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return guesses[section].count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let margin: CGFloat = 20
+        let size: CGFloat = (collectionView.frame.size.width - margin) / 5
+        
+        return CGSize(width: size, height: size)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 2,
+                            left: 2,
+                            bottom: 2,
+                            right: 2)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
 }
